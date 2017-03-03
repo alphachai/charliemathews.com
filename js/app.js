c08141b58546332ce05b4d085196a1d4476c95a6
Charlie Mathews <charles@parkes.io>
2017-03-03 00:46:45 -0600

    Initial commit. Working concept.

---
+++ js/app.js
@@ -0,0 +1,39 @@
+//particlesJS.load('particles', 'assets/particles.json');
+
+// handle loading animation
+$(document).ready(function($) {
+	$("#preloader-wrap").animate({opacity: 0}, 400);
+	setTimeout(function() {$("#loading").animate({opacity: 0}, 200);}, 400);
+	setTimeout(function() {$("#loading").remove();}, 600);
+});
+
+function showMenu() {
+	$("#burger").addClass("cross");
+	$("#menu").addClass("open");
+	$("article").css("opacity", "0.2");
+}
+
+function hideMenu() {
+	$("#burger").removeClass("cross");
+	$("#menu").removeClass("open");
+	$("article").css("opacity", "1");
+}
+
+// menu animation
+$("#burger").click(function() {
+  if(!$(this).hasClass("cross")) {
+		showMenu();
+	} else {
+		hideMenu();
+	}
+});
+
+// hide menu if user clicks on content? need to improve.
+$("#content").click(function() {
+  if($("#burger").hasClass("cross")) hideMenu();
+});
+
+// hide menu if user scrolls
+$(window).scroll(function() {
+  if($("#burger").hasClass("cross")) hideMenu();
+});
